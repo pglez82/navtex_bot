f0d5ef8ffb664ba589de90b591d5b03697dcbcdc
pglez82@gmail.com <pglez82@gmail.com>
2019-03-21 09:54:08 +0100

    Initial commit

---
+++ navtex.py
@@ -0,0 +1,171 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+import re
+import xml.etree.ElementTree
+from datetime import datetime
+import pytz
+import schedule
+import time
+from dateutil import tz
+from threading import Thread
+
+
+
+class NavtexSchedule:
+    def __init__(self, logger):
+        self.metareas = xml.etree.ElementTree.parse('navtex.xml').getroot()
+        self.logger = logger
+
+    def get_metareas(self):
+        ids = []
+        for metarea in self.metareas.findall('metarea'):
+            ids.append({'id':metarea.get('id'),'url':metarea.get('url'),'description':metarea.get('description')})
+        return ids
+
+    def get_area_messages(self,metarea):
+        metareaxml=self.metareas.find("metarea[@id='"+metarea+"']")
+        messagesxml = metareaxml.getchildren()[0]
+        messages = []
+        for messagexml in messagesxml:
+            times = []
+            for time in messagexml.getchildren()[0]:
+                times.append(datetime.strptime(time.text,"%H:%M:%S").replace(tzinfo=pytz.UTC))
+            messages.append({'id':messagexml.get('id'),'times':times})
+
+        return messages
+
+    def get_message_times(self,message):
+        messagexml=self.metareas.find(".//message[@id='"+message+"']")
+        times = []
+        for time in messagexml.getchildren()[0]:
+            times.append(datetime.strptime(time.text,"%H:%M:%S").replace(tzinfo=pytz.UTC))
+        return times
+
+
+class NavtexScrapper:
+    def __init__(self,logger):
+        self.logger = logger
+
+    def simple_get(self,url):
+        """
+        Attempts to get the content at `url` by making an HTTP GET request.
+        If the content-type of response is some kind of HTML/XML, return the
+        text content, otherwise return None.
+        """
+        try:
+            with closing(get(url, stream=True)) as resp:
+                if self.is_good_response(resp):
+                    return resp.content
+                else:
+                    return None
+
+        except RequestException as e:
+            self.log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+            return None
+
+    def is_good_response(self,resp):
+        """
+        Returns True if the response seems to be HTML, False otherwise.
+        """
+        content_type = resp.headers['Content-Type'].lower()
+        return (resp.status_code == 200
+                and content_type is not None
+                and content_type.find('html') > -1)
+
+    def log_error(self,e):
+        """
+        It is always a good idea to log errors.
+        This function just prints them, but you can
+        make it do anything.
+        """
+        self.logger.error(e)
+
+    #Example 'http://weather.gmdss.org/III.html'
+    def get_bulletin(self,url,message):
+        """
+        Gets a bulletin and returns it as text
+        :param url: the url of the area
+        :param message: the id of the message
+        :return: the text of the bulletin
+        """
+        raw_html = self.simple_get(url)
+        if raw_html != None:
+            html = BeautifulSoup(raw_html, 'html.parser')
+            link = html.find('a', attrs={'href': re.compile("^bulletins/"+message)})
+            linkp=link.get('href')
+            bulletinurl = "http://weather.gmdss.org/"+linkp.replace(".html",".txt")
+            return get(bulletinurl).content.decode('latin1')
+        else:
+            return None
+
+
+class NavtexDowloader(Thread):
+    """
+    This object schedules the download of all the bulletins at the corresponding time using a new thread for each
+    """
+    def download(self, url, message, retry=False):
+        self.logger.info("Downloading message "+message)
+        messagecontent = self.scrapper.get_bulletin(url,message)
+        if messagecontent != None:
+            self.logger.info("Message ["+message+"] downloaded properly.")
+
+            #Remove annoying breaklines
+            messagecontent = re.sub(r'(\w)\n(\w)', r'\1 \2', messagecontent) #being in brackets we form a group. We can make a reference to that group using \1
+            #We store the message
+            self.messages[message] = messagecontent
+
+            #We call the callback in order to send the message by telegram
+            if self.send_messages:
+                self.function_callback(message,messagecontent)
+            #This job was just lanched after a network fail so we cancel it after finnishing it
+            if retry:
+                self.logger.info("This job has been just lanched to redownload a message ["+message+"] that failed. We got the message so we cancel it")
+                return schedule.CancelJob
+        else:
+            if retry:
+                self.logger.error("We are retrying but we still where not able to download the message ["+message+"]. Thread should retry again in one minute.")
+            else:
+                self.logger.error("Message ["+message+"] could not be downloaded. Reescheduling the job every 1 minute to try to download the message")
+                schedule.every(1).minutes.do(self.download, url, message, True)
+
+
+
+    def get_stored_message(self,message):
+        if message not in self.messages:
+            return "The message do not exist in the server. Please wait until the message is downloaded again."
+        else:
+            return self.messages[message]
+
+    def time_to_localtime(self,t):
+        return t.astimezone(tz.tzlocal()).strftime('%H:%M')
+
+    def __init__(self,function_callback, logger):
+        """
+        Checks the schedule (from the xml) and schedules all the downloads in order to be executed. The threads gets then blocked
+        """
+        super().__init__()
+        self.logger = logger
+
+        self.send_messages = False
+
+        self.function_callback = function_callback
+        self.messages = {}
+
+        s = NavtexSchedule(logger)
+        self.scrapper = NavtexScrapper(logger)
+        for metarea in s.get_metareas():
+            for message in s.get_area_messages(metarea['id']):
+                for t in message['times']:
+                    schedule.every().day.at(self.time_to_localtime(t)).do(self.download,metarea['url'],message['id'])
+
+
+    def run(self):
+        #schedule.run_all(5)
+        self.send_messages = True
+        while 1:
+            schedule.run_pending()
+            time.sleep(5)
+
+
---
+++ navtexbot.py
@@ -0,0 +1,233 @@
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, PicklePersistence
+import logging
+import navtex
+import sys
+
+
+class NavtexBot:
+
+    # Define a few command handlers. These usually take the two arguments bot and
+    # update. Error handlers also receive the raised TelegramError object in error.
+    def help(self,bot, update):
+        """Send a message when the command /help is issued."""
+        update.message.reply_text('This is the list of commands that you can use:\n'+
+                                  '/listmetareas - lists all the Navtex METAREAS supported by the bot\n'+
+                                  '/subscribe2messages - Subscribe to Navtex messages. You will chose an area and you will receive messages from this area\n'+
+                                  '/unsubscribe2messages - Stop receiving messages from an area\n'+
+                                  '/listsubscriptions - Check the messages you are subscribed to\n'+
+                                  '/getmessages - Get messages from your subscriptions. Do not wait until messages are automatically sent to you. You will get the last message available.')
+
+    def start(self,bot,update):
+        update.message.reply_text('Welcome! You can check the commands available with the command /help.')
+
+    def listmetareas(self, bot, update):
+        reply = ""
+        for metarea in self.metareas:
+            reply = reply + 'METAREA ' + metarea['id'] + ' - ' + metarea['description']+'\n'
+        update.message.reply_text(reply)
+
+
+    def subscribe2messages(self,bot, update, user_data):
+        buttons = []
+        buttons.append([])
+        for metarea in self.metareas:
+            buttons[0].append(InlineKeyboardButton("METAREA "+metarea['id'], callback_data='MA'+metarea['id']))
+
+        reply_markup = InlineKeyboardMarkup(buttons)
+
+        update.message.reply_text('Please choose a METAREA:', reply_markup=reply_markup)
+
+
+    def unsubscribe2messages(self,bot, update, user_data):
+        if "subscriptions" not in user_data or not user_data["subscriptions"]:
+            reply="You are not subscribed to any Navtex message. Nothing to unsubscribe!"
+            update.message.reply_text(reply)
+        else:
+            reply="Choose the message to unsubscribe to: "
+            buttons = []
+            for message in user_data['subscriptions']:
+                buttons.append([InlineKeyboardButton('X ' + message, callback_data='UMSG'+message)])
+            reply_markup = InlineKeyboardMarkup(buttons)
+            update.message.reply_text(reply, reply_markup=reply_markup)
+
+    def listsubscriptions(self,bot, update, user_data):
+        if "subscriptions" not in user_data or not user_data['subscriptions']:
+            reply="You are not subscribed to any Navtex message. Use the command /subscribe2messages in order to subscribe."
+        else:
+            reply="You are subscribed to:\n"
+            for message in user_data['subscriptions']:
+                times = self.s.get_message_times(message)
+                reply=reply + message
+                for time in times:
+                    reply = reply + ' ' + time.strftime('%H:%M')
+                reply = reply + ' (UTC)\n'
+
+        update.message.reply_text(reply)
+
+
+    def buttonhandler_metareas(self,bot, update, user_data):
+        query = update.callback_query
+        metarea = query.data[2:]
+        messages = self.s.get_area_messages(metarea)
+        buttons = []
+        for message in messages:
+            buttons.append([InlineKeyboardButton(message['id'],callback_data='MSG'+message['id'])])
+
+        reply_markup = InlineKeyboardMarkup(buttons)
+        bot.edit_message_text(text="Selected METAREA {}. Choose the message you want to subscribe:".format(metarea),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          reply_markup=reply_markup)
+
+
+    def buttonhandler_subscribe(self,bot, update, user_data):
+        query = update.callback_query
+        message = query.data[3:]
+        times = self.s.get_message_times(message)
+        if self.add_user_subscription(message,user_data,query.message.chat_id):
+            reply = "You are now subscribed to messages in "+message+". These messages will arrive at"
+            for time in times:
+                reply = reply + ' ' + time.strftime('%H:%M')
+            reply = reply + ' (UTC)'
+            bot.edit_message_text(text=reply,
+                              chat_id=query.message.chat_id,
+                              message_id=query.message.message_id)
+        else:
+            bot.edit_message_text(text="You were already subscribed to this message",
+                              chat_id=query.message.chat_id,
+                              message_id=query.message.message_id)
+
+    def buttonhandler_unsubscribe(self,bot, update, user_data):
+        query = update.callback_query
+        message = query.data[4:]
+
+        self.remove_user_subscription(message, user_data)
+        reply = "Succesfully unsubscribed from  "+message
+        bot.edit_message_text(text=reply,
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+
+    def getmessages(self,bot, update, user_data):
+        for message in user_data['subscriptions']:
+            msg = self.downloader.get_stored_message(message)
+            self.send_message(bot,update.message.chat_id,msg)
+
+    def add_user_subscription(self, message, user_data, chat_id):
+        if "subscriptions" not in user_data:
+            user_data['subscriptions'] = []
+        if message in user_data['subscriptions']:
+            return False
+        else:
+            user_data['subscriptions'].append(message)
+            user_data['chat_id']=chat_id
+            self.logger.info("User with chat_id="+str(user_data['chat_id'])+" has subscribed to "+message)
+            self.my_persistence.flush()
+            return True
+
+    def remove_user_subscription(self, message, user_data):
+        user_data['subscriptions'].remove(message)
+        self.logger.info("User with chat_id="+str(user_data['chat_id'])+" has removed the subscription to message "+message)
+        self.my_persistence.flush()
+
+    def new_message_received(self,messageid,messagecontent):
+        self.logger.info("New message received: "+messageid+". Computing users to send it to.")
+        #Compute to which users we need to send the messages
+        user_data=self.my_persistence.get_user_data()
+        chat_ids = []
+        for key, value in user_data.items():
+            if messageid in value['subscriptions']:
+                chat_ids.append(value['chat_id'])
+        self.logger.info("Sending message to these users: "+str(chat_ids))
+
+        message_number = 0
+        for chat_id in chat_ids:
+            self.logger.info("Sending message "+messageid+" to "+str(chat_id))
+            #We cant send more than 30 messages per second, so we should count how many we are sending. We are going to be conservative. only 20 messages per second
+            job_queue = self.updater.job_queue
+            job_queue.run_once(lambda bot, job, chat_id=chat_id: (self.send_message(bot,chat_id,messagecontent)), message_number//20)
+            message_number = message_number + len(messagecontent)//telegram.constants.MAX_MESSAGE_LENGTH+1
+
+    def send_message(self, bot, chat_id, text: str, **kwargs):
+        if len(text) <= telegram.constants.MAX_MESSAGE_LENGTH:
+            return bot.send_message(chat_id, text, **kwargs)
+
+        parts = []
+        while len(text) > 0:
+            if len(text) > telegram.constants.MAX_MESSAGE_LENGTH:
+                part = text[:telegram.constants.MAX_MESSAGE_LENGTH]
+                first_lnbr = part.rfind('\n')
+                if first_lnbr != -1:
+                    parts.append(part[:first_lnbr])
+                    text = text[first_lnbr:]
+                else:
+                    parts.append(part)
+                    text = text[telegram.constants.MAX_MESSAGE_LENGTH:]
+            else:
+                parts.append(text)
+                break
+
+        msg = None
+        part_count = 0
+        for part in parts:
+            part_count = part_count + 1
+            self.logger.info("Sending part "+str(part_count)+"to user "+str(chat_id))
+            msg = bot.send_message(chat_id, part, **kwargs)
+        return msg
+
+    def error(self,bot, update, error):
+        """Log Errors caused by Updates."""
+        self.logger.warning('Update "%s" caused error "%s"', update, error)
+
+    def __init__(self):
+        #Configure logging
+        # Enable logging
+        if len(sys.argv)<2:
+            print("You need to pass a telegram api key as an argument...")
+            exit(-1)
+
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+
+        self.logger.info("Arrancando el bot")
+
+        self.s = navtex.NavtexSchedule(self.logger)
+        self.metareas = self.s.get_metareas()
+        self.my_persistence = PicklePersistence(filename='user_data',store_chat_data=False,on_flush=True)
+        self.updater = Updater(sys.argv[1],persistence=self.my_persistence)
+        self.downloader = navtex.NavtexDowloader(self.new_message_received,self.logger)
+
+        # Get the dispatcher to register handlers
+        dp = self.updater.dispatcher
+
+        # on different commands - answer in Telegram
+        dp.add_handler(CommandHandler("help", self.help))
+        dp.add_handler(CommandHandler("start", self.start))
+        dp.add_handler(CommandHandler("listmetareas", self.listmetareas))
+        dp.add_handler(CommandHandler("subscribe2messages", self.subscribe2messages,pass_user_data=True))
+        dp.add_handler(CommandHandler("unsubscribe2messages", self.unsubscribe2messages,pass_user_data=True))
+        dp.add_handler(CommandHandler("getmessages", self.getmessages,pass_user_data=True))
+        dp.add_handler(CommandHandler("listsubscriptions", self.listsubscriptions,pass_user_data=True))
+        dp.add_handler(CallbackQueryHandler(self.buttonhandler_metareas,pattern='MA',pass_user_data=True))
+        dp.add_handler(CallbackQueryHandler(self.buttonhandler_subscribe,pattern='MSG',pass_user_data=True))
+        dp.add_handler(CallbackQueryHandler(self.buttonhandler_unsubscribe,pattern='UMSG',pass_user_data=True))
+
+
+
+        # log all errors
+        dp.add_error_handler(self.error)
+
+        # Start the Bot
+        self.updater.start_polling()
+
+        #Start the scrapper
+        self.downloader.start()
+
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start_polling() is non-blocking and will stop the bot gracefully.
+        self.updater.idle()
+
+
+NavtexBot()
